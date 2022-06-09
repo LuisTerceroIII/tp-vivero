5e0fcdf7351af6e7ac463557defb7ba7c2fbedfd
Marcos <marcoscar2001@gmail.com>
2022-06-09 13:57:10 -0300

    pronostico con botones, hacerlo Accordion

--- js/climaService.js
+++ js/climaService.js
@@ -1,77 +1,98 @@
+
 const main = async () => {
 
-    const fetchURL = async (url) => {
-      const res = await fetch(`https://weatherservices.herokuapp.com/api/${url}`)
-      return res.json()
-    }
-    
-    const { locations: [sanMiguelLocation] } = await fetchURL("locations")
-    const wheaterDataSanMiguel = await fetchURL(`weather/${sanMiguelLocation.state_id}`)
-    const {item : { weather }} = wheaterDataSanMiguel
-    const {forecast:{ forecast: {0: saturday, 1: sunday, 2: monday, 3: tuesday} }} = wheaterDataSanMiguel.item
-    const forecast = [saturday, sunday, monday, tuesday]
+  const fetchURL = async (url) => {
+    const res = await fetch(`https://weatherservices.herokuapp.com/api/${url}`)
+    return res.json()
+  }
 
-    const extForecast = () => {
-      return forecast.map((day, i) => {
-        return `
+  const { locations: [sanMiguelLocation] } = await fetchURL("locations")
+  const wheaterDataSanMiguel = await fetchURL(`weather/${sanMiguelLocation.state_id}`)
+  const { item: { weather } } = wheaterDataSanMiguel
+  const { forecast: { forecast: { 0: saturday, 1: sunday, 2: monday, 3: tuesday } } } = wheaterDataSanMiguel.item
+  const forecast = [saturday, sunday, monday, tuesday]
+
+  const extForecast = () => {
+    return forecast.map((day, i) => {
+      return `
         <div class="forecast-extend-item">
         
           <div class="forecast-inner-item">
           <span class="forecast-detail-item-date"><p id="date-${i}">${day.date}</p></span>
-            <span class="forecast-detail-item">Temp min:<p id="min-temp-${i}">${day.temp_min ? `${day.temp_min}°C` : " ? °C " }</p></span>
+            <span class="forecast-detail-item">Temp min:<p id="min-temp-${i}">${day.temp_min ? `${day.temp_min}°C` : " ? °C "}</p></span>
             <span class="forecast-detail-item">Temp max:<p id="max-temp-${i}">${day.temp_max ? `${day.temp_max}°C` : " ? °C "}</p></span>
           </div>
         </div>
       `
-      })
-    }
-    const forecastContainer = document.getElementById('extend-forecast-main-container')
-    extForecast().forEach(forecast => forecastContainer.appendChild(createElementFromHTML(forecast)))
-    
-    const temp = document.getElementById('tempeture')
-    const minTemp = document.getElementById('min-tempeture')
-    const humidity = document.getElementById('humidity')
-    const pressure = document.getElementById('pressure')
-    const visibility = document.getElementById('visibility')
-    const windSpeed = document.getElementById('wind_speed')
-    const windDeg = document.getElementById('wind_deg')
-    const description = document.getElementById('description')
-    const image = document.getElementById('image')
-
-    const location = document.getElementById('location')
-    location.innerHTML = `${sanMiguelLocation.name}, ${sanMiguelLocation.province}`
-
-    temp.innerHTML = `${weather.temp}°C`;
-    minTemp.innerHTML = `${weather.tempDesc}`;
-    humidity.innerHTML = `${weather.humidity}%`;
-    pressure.innerHTML = `${weather.pressure}hPa`;
-    visibility.innerHTML = `${weather.visibility}km`;
-    windSpeed.innerHTML = `${weather.wind_speed}Km`;
-    windDeg.innerHTML = `${weather.wing_deg}`;
-    description.innerHTML = `${weather.description}`;
-    image.src = './../img/sun.svg'
-
-    const { alerts: alertsDayOne } = await fetchURL("alerts/byDay/1")
-    const { alerts: alertsDayTwo } = await fetchURL("alerts/byDay/2")
-    const { alerts: alertsDayThree } = await fetchURL("alerts/byDay/3")
-
-    const alerts = [alertsDayOne, alertsDayTwo, alertsDayThree]
-    
-    const alertsContainer = document.getElementById('alerts-main-container')
-    let alertsToShow = []
-    alerts.forEach(day => {
-      let alertsByDay = day.map(alert => {
-        const zones = Object.keys(alert.zones)
-        return `<div class="alert-item">
-        <div class="date-and-hour-alert">Fecha: ${alert.date}&nbsp;&nbsp;&nbsp;Comienza: ${alert.hour.slice(0,5)}</div>
-        <h1 class="alert-subtitle">Descripcion: </h1>
-        <p>${alert.description ? alert.description : "" }</p>
-        <h1 class="alert-subtitle">Zonas afectadas: </h1>
-        <p>${zones.map(zone => alert.zones[zone])}</p>
-      </div>`
-      })
-      alertsToShow = [...alertsToShow, ...alertsByDay]
     })
-    alertsToShow.forEach(alert => alertsContainer.appendChild(createElementFromHTML(alert)))
   }
-  document.addEventListener("DOMContentLoaded", main)
+  const forecastContainer = document.getElementById('extend-forecast-main-container')
+  extForecast().forEach(forecast => forecastContainer.appendChild(createElementFromHTML(forecast)))
+
+  const temp = document.getElementById('tempeture')
+  const minTemp = document.getElementById('min-tempeture')
+  const humidity = document.getElementById('humidity')
+  const pressure = document.getElementById('pressure')
+  const visibility = document.getElementById('visibility')
+  const windSpeed = document.getElementById('wind_speed')
+  const windDeg = document.getElementById('wind_deg')
+  const description = document.getElementById('description')
+  const image = document.getElementById('image')
+
+  const location = document.getElementById('location')
+  location.innerHTML = `${sanMiguelLocation.name}, ${sanMiguelLocation.province}`
+
+  temp.innerHTML = `${weather.temp}°C`;
+  minTemp.innerHTML = `${weather.tempDesc}`;
+  humidity.innerHTML = `${weather.humidity}%`;
+  pressure.innerHTML = `${weather.pressure}hPa`;
+  visibility.innerHTML = `${weather.visibility}km`;
+  windSpeed.innerHTML = `${weather.wind_speed}Km`;
+  windDeg.innerHTML = `${weather.wing_deg}`;
+  description.innerHTML = `${weather.description}`;
+  image.src = './../img/sun.svg'
+
+  const { alerts: alertsDayOne } = await fetchURL("alerts/byDay/1")
+  const { alerts: alertsDayTwo } = await fetchURL("alerts/byDay/2")
+  const { alerts: alertsDayThree } = await fetchURL("alerts/byDay/3")
+
+  const alerts = [alertsDayOne, alertsDayTwo, alertsDayThree]
+  const alertsContainer = document.getElementById('alerts-main-container')
+
+  const btns = []; //new
+
+  alerts.forEach((e, i) => { //new
+    let divDia = createElementFromHTML(`<div id="alerts/byDay/${i}" class="contenedor"> <div id="info-day-${i}" class="info-day"></div></div>`);
+    alertsContainer.appendChild(divDia);
+    let boton = createElementFromHTML(`<button class="bntClima " id="btnDay${i}" type="submit">Día ${i+1}</button>"`)
+    btns.push(boton);
+    document.getElementById(`alerts/byDay/${i}`).append(boton);
+  })
+
+  btns.forEach((e, i) => { //new
+    e.addEventListener("click", () => {
+      let botonapretado = i;
+      console.log(botonapretado);
+      alerts[i].forEach((c) => {
+        document.getElementById(`info-day-${i}`).innerHTML += `
+          <p><span> Fecha: </span>${c.date}</p>
+          <p><span> Hora: </span>${c.hour}</p>
+          <p><span> Titulo: </span> ${c.title}</p>
+          <p><span> ${c.status == null ? '' : "Estado: "} </span>${c.status == null ? '' : c.status}</p>
+          <p><span> ${c.description == null ? '' : "Descripción:"} </span>${c.description == null ? '' : c.description}</p>
+          <br> <br> <br>
+          `;
+        e.disabled = "true";
+      })
+      
+    })
+
+    //Tengo que tener todas las referencias a los divs para ir aplicandole y quitandole clases a los 3
+    // los 3 divs se basan en la id "info-day-i" i=0,1,2, no tengo la referencia en variables.
+
+  })
+
+}
+
+
+document.addEventListener("DOMContentLoaded", main)
